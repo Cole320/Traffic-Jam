6c1d1e865d0cf80e3b5d20fdb865a32f2b1b8224
Coal <44954117+Cole320@users.noreply.github.com>
2021-10-13 16:18:08 +0000

    .

--- traffic-jam.js
+++ traffic-jam.js
@@ -1,13 +1,20 @@
 alert(1);
-(function() {
+
+function getUrl()
+{
     // Get Desired URL From User
     var rawUrl = prompt("\
-                    Welcome to Traffic Jam <3\n\
-                    Enter a URL to get started:\n\n\n\
-                    -<3");
+    Welcome to Traffic Jam <3\n\
+    Enter a URL to get started:\n\n\n\
+    <3");
     
+    // Format URL
     if ( rawUrl.startsWith("https://") || rawUrl.startsWith("http://") ) { url = rawURL; }
-    else { url = "https://" + rawUrl }
+    else if ( rawUrl ) { url = "https://" + rawUrl; }
+    else { alert("URL Invalid"); return getUrl(); }
 
-    alert(url);
-});
+    return url;
+}
+
+
+alert(url);
