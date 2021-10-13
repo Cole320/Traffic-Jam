1a5969275b5b68b3e265efc75a717ad642b0c6d9
Coal <44954117+Cole320@users.noreply.github.com>
2021-10-13 16:05:49 +0000

    Second commit, mainly testing things for now

--- bootstrap.js
+++ bootstrap.js
@@ -1,4 +1,2 @@
-  # Bootstrap Bookmarklet:
-  ```
 javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://raw.githubusercontent.com/Cole320/Traffic-Jam/main/traffic-jam.js'})()
-  ```
+// 
---
+++ traffic-jam.js
@@ -0,0 +1,13 @@
+/* javascript: */
+(function() {
+    // Get Desired URL From User
+    var rawUrl = prompt("\
+                    Welcome to Traffic Jam <3\n\
+                    Enter a URL to get started:\n\n\n\
+                    -<3");
+    
+    if ( rawUrl.startsWith("https://") || rawUrl.startsWith("http://") ) { url = rawURL; }
+    else { url = "https://" + rawUrl }
+
+    alert(url);
+});
